902b6cfe0c702f922e9bbdba3a8215a747ffaa9a
Miguel Contreras <macc07112005c80@gmail.com>
2024-05-15 19:33:11 -0500

    Settings

--- scramblies.py
+++ scramblies.py
@@ -12,10 +12,9 @@ scramble('cedewaraaossoqqyt', 'codewars') ==> True
 scramble('katas', 'steak') ==> False
 """
 
-# def scramble(s1,s2):
-#     for c in set(s2):
-#         if s1.count(c) < s2.count(c):
-#             return False
-#     return True
+def scramble(s1,s2):
+    for c in set(s2):
+        if s1.count(c) < s2.count(c):
+            return False
+    return True
 
-print(sorted(set("abcdef")))
